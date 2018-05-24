c10618ca895b23e0d95770d440a0493dc8c8b666
ethanryan <ethanryan@gmail.com>
2018-05-24 11:04:30 -0400

    added components folder, added TextEdit component, rendering from App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import TextEdit from './components/TextEdit.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to TextEdit-App</h1>
+          <h1>Welcome to TextEdit-App</h1>
         </header>
+
+        <TextEdit />
 
       </div>
     );
---
+++ src/components/TextEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TextEdit extends Component {
+  render() {
+    return (
+      <div>
+        Here's the TextEdit component.
+      </div>
+    );
+  }
+}
+
+export default TextEdit;
