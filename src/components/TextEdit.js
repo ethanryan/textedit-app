191d643c75bb60cb20f01ec64823acaf88dba5ec
ethanryan <ethanryan@gmail.com>
2018-06-06 00:14:05 -0400

    still working on displaying one room at a time

--- src/components/TextEdit.js
+++ src/components/TextEdit.js
@@ -18,39 +18,14 @@ var link = 'http://localhost:1234'
 // create a connection
 var connection = io(link) //need to include LINK within io()...
 
-///for room3::::::::
-///for room3::::::::
-///for room3::::::::
-// Y({
-//   db: {
-//     name: 'memory' // use the memory db adapter
-//   },
-//   connector: {
-//     name: 'websockets-client', // use the websockets-client connector
-//     // room: 'room3',
-//     room: this.props.showRoom,
-//     socket: connection, //passing connection above as the socket...
-//     url: link // the connection endpoint (see y-websockets-server)
-//   },
-//   share: {
-//     textarea: 'Text' // y.share.textarea is of type Y.Text
-//   }
-// }).then(function (y) {
-//   // bind the textarea to a shared text element
-//   // y.share.textarea.bind(document.getElementById('room3'))
-//   y.share.textarea.bind(document.getElementById(this.props.showRoom))
-//   // y.share.textarea.bind(document.querySelector('textarea'))
-// })
-
-
-
 
 
 class TextEdit extends Component {
 
   componentDidMount() {
-    console.log('TextEdit - componentDidMount - this.props is: ...', this.props)
-    var that = this; //setting 'this' to 'that' so scope of 'this' doesn't get lost in promise below
+    console.log('TextEdit - componentDidMount - this.props is: ', this.props)
+    // var that = this; //setting 'this' to 'that' so scope of 'this' doesn't get lost in promise below
+    var currentRoom = this.props.showRoom;
 
     Y({
       db: {
@@ -59,7 +34,7 @@ class TextEdit extends Component {
       connector: {
         name: 'websockets-client', // use the websockets-client connector
         // room: 'room3',
-        room: this.props.showRoom,
+        room: currentRoom, //this will get updated when handleRoomChange is called...
         socket: connection, //passing connection above as the socket...
         url: link // the connection endpoint (see y-websockets-server)
       },
@@ -68,7 +43,7 @@ class TextEdit extends Component {
       }
     }).then(function (y) {
       // bind the textarea to a shared text element
-      y.share.textarea.bind(document.getElementById(that.props.showRoom))
+      y.share.textarea.bind(document.getElementById(currentRoom))
       // y.share.textarea.bind(document.querySelector('textarea'))
     })
 
@@ -78,78 +53,63 @@ class TextEdit extends Component {
     console.log('TextEdit - componentWillReceiveProps - nextProps is: ...', nextProps)
 
     if (nextProps.showRoom !== this.props.showRoom) {
-      console.log('>>>>>>>PROPS ARE DIFFERENT ~~~ FUCK WE GOTTA DO SOMETHING!!!!')
-      console.log('nextProps.showRoom is: ', nextProps.showRoom)
-      console.log('this.props.showRoom is: ', this.props.showRoom)
+      console.log('0-->>>PROPS ARE DIFFERENT ~~~ WE GOTTA FIX IT!!!')
+      console.log('0.nextProps.showRoom is: ', nextProps.showRoom)
+      console.log('0.this.props.showRoom is: ', this.props.showRoom)
+      // this.props.handleRoomChange(nextProps.showRoom) //passing 'newRoom' here to parent...
+      //seeing what happens when we call this.... //this is onButtonClick...
+    } //end if statement...
+  }
 
-      // var that = this; //setting 'this' to 'that' so scope of 'this' doesn't get lost in promise below
-      //
-      // Y({
-      //   db: {
-      //     name: 'memory' // use the memory db adapter
-      //   },
-      //   connector: {
-      //     name: 'websockets-client', // use the websockets-client connector
-      //     // room: 'room3',
-      //     room: nextProps.showRoom, //not sure about this.... <<<<<------------------------
-      //     socket: connection, //passing connection above as the socket...
-      //     url: link // the connection endpoint (see y-websockets-server)
-      //   },
-      //   share: {
-      //     textarea: 'Text' // y.share.textarea is of type Y.Text
-      //   }
-      // }).then(function (y) {
-      //   // bind the textarea to a shared text element
-      //   y.share.textarea.bind(document.getElementById(nextProps.showRoom)) //not sure about this....  <<<<<------------------------
-      //   // y.share.textarea.bind(document.querySelector('textarea'))
-      // })
+  shouldComponentUpdate(nextProps) {
+    console.log('TextEdit - shouldComponentUpdate - nextProps is: ...', nextProps)
+
+    if (nextProps.showRoom !== this.props.showRoom) {
+      console.log('1-->>>PROPS ARE DIFFERENT ~~~ WE GOTTA FIX IT!!!')
+      console.log('1.nextProps.showRoom is: ', nextProps.showRoom)
+      console.log('1.this.props.showRoom is: ', this.props.showRoom)
+      console.log('returning TRUE')
+      return true
+      // this.props.handleRoomChange(nextProps.showRoom) //passing 'newRoom' here to parent...
+      //seeing what happens when we call this.... //this is onButtonClick...
     } //end if statement...
   }
 
   render() {
-    console.log('in TextEdit, this.props is: ', this.props)
+    // console.log('in TextEdit, this.props is: ', this.props)
+
+    const fullWidth = {
+      width:'100%',
+    }
 
     return (
       <div>
 
         <h3>
-          Here's the TextEdit component.
+          TextEdit component
         </h3>
 
+        <p>
+          {this.props.showRoom ? this.props.showRoom : "room number goes here..."}
+        </p>
+
         <div>
-        <span>
-          # {this.props.showRoom ? this.props.showRoom : "room number goes here..."}:
-        </span>
 
           <textarea
             id={this.props.showRoom ? this.props.showRoom : "room name goes here..."}
             // name={this.props.showRoom ? this.props.showRoom : "room name goes here..."}
+            style={fullWidth}
             rows="2"
             cols="80"
             placeholder="textarea inside TextEditContainer..."
             >
-          </textarea>
+            </textarea>
+
+          </div>
 
         </div>
-
-        {/* <div>
-        Room 1: //this.props.room or whatever...
-          <textarea
-            id="room1"
-            rows="2"
-            cols="80"
-            placeholder="textarea inside TextEditContainer..."
-            // value={this.state.text}
-            value={this.state.room1}
-            onChange={this.handleTextChange}
-            name="room1"
-            >
-          </textarea>
-        </div> */}
-
-      </div>
-    );
+      );
+    }
   }
-}
 
-export default TextEdit;
+  export default TextEdit;
--- src/container/TextEditContainer.js
+++ src/container/TextEditContainer.js
@@ -3,78 +3,6 @@ import React, { Component } from 'react';
 // import Clock from '../components/Clock.js';
 import TextEdit from '../components/TextEdit.js';
 
-// import io from "socket.io-client"; //this and below via: https://blog.cloudboost.io/creating-a-chat-web-app-using-express-js-react-js-socket-io-1b01100a8ea5
-
-// const socket = io('localhost:8080'); //initiate the request to open a socket connection with our Express server, in server.js
-
-// const Y = require('yjs')
-
-// Yjs plugins
-// require('y-memory')(Y)
-// require('y-array')(Y)
-// require('y-text')(Y)
-// require('y-websockets-client')(Y) //i imagine i need to require this too...
-// //will also need a connector here... not y-ipfs-connector, but something with socket.io
-//
-// var io = Y['websockets-client'].io //need to get this.....
-//
-//
-// var link = 'http://localhost:1234'
-// // var link = 'https://textarea-yjs-websockets-server.herokuapp.com/'
-//
-// // create a connection
-// var connection = io(link) //need to include LINK within io()...
-
-
-// //for room1::::::::
-// //for room1::::::::
-// //for room1::::::::
-// Y({
-//   db: {
-//     name: 'memory' // use the memory db adapter
-//   },
-//   connector: {
-//     name: 'websockets-client', // use the websockets-client connector
-//     // room: 'Textarea-example-dev',
-//     room: 'room1',
-//     socket: connection, //passing connection above as the socket...
-//     url: link // the connection endpoint (see y-websockets-server)
-//   },
-//   share: {
-//     textarea: 'Text' // y.share.textarea is of type Y.Text
-//   }
-// }).then(function (y) {
-//   // bind the textarea to a shared text element
-//   y.share.textarea.bind(document.getElementById('room1'))
-//   // y.share.textarea.bind(document.querySelector('textarea'))
-// })
-//
-// ///for room2::::::::
-// ///for room2::::::::
-// ///for room2::::::::
-// Y({
-//   db: {
-//     name: 'memory' // use the memory db adapter
-//   },
-//   connector: {
-//     name: 'websockets-client', // use the websockets-client connector
-//     room: 'room2',
-//     socket: connection, //passing connection above as the socket...
-//     url: link // the connection endpoint (see y-websockets-server)
-//   },
-//   share: {
-//     textarea: 'Text' // y.share.textarea is of type Y.Text
-//   }
-// }).then(function (y) {
-//   // bind the textarea to a shared text element
-//   y.share.textarea.bind(document.getElementById('room2'))
-//   // y.share.textarea.bind(document.querySelector('textarea'))
-// })
-
-
-
-
-
 
 
 class TextEditContainer extends Component {
@@ -83,15 +11,14 @@ class TextEditContainer extends Component {
     this.state = {
       timestamp: '',
       text: '',
-      choice: '',
-      // room1: '', //this or text, above??
-      // room2: '', //this or text, above??
-      // showRoom: '', //default
+      // choice: '',
       showRoom: 'room1', //default
+      // showRoom: 'room4', //default
     };
     // this.handleTextChange = this.handleTextChange.bind(this)
-    this.onChoiceClick = this.onChoiceClick.bind(this)
+    // this.onChoiceClick = this.onChoiceClick.bind(this)
     this.onButtonClick = this.onButtonClick.bind(this)
+    // this.handleRoomChange = this.handleRoomChange.bind(this)
   } //end constructor
 
   // componentDidMount() {
@@ -106,21 +33,27 @@ class TextEditContainer extends Component {
   //   this.setState({ [event.target.name]: textValue });
   // }
 
-  onChoiceClick = event => {
-    // console.log('button event.target.name: ', event.target.name)
-    const buttonValue = event.target.name
-    console.log('buttonValue: ', buttonValue)
-    this.setState({ choice: buttonValue });
-    console.log('setState to choice: ', buttonValue)
-  }
+  // onChoiceClick = event => {
+  //   // console.log('button event.target.name: ', event.target.name)
+  //   const buttonValue = event.target.name
+  //   console.log('onChoiceClick - buttonValue: ', buttonValue)
+  //   this.setState({ choice: buttonValue });
+  //   // console.log('setState to choice: ', buttonValue)
+  // }
 
   onButtonClick = event => {
     // console.log('button event.target.name: ', event.target.name)
     const buttonValue = event.target.name
     console.log('buttonValue: ', buttonValue)
     this.setState({ showRoom: buttonValue });
-    console.log('setState to showRoom: ', buttonValue)
+    // console.log('setState to showRoom: ', buttonValue)
   }
+
+  // handleRoomChange(newRoom) {
+  //   console.log('***--> handleRoomChange, newRoom is: ', newRoom)
+  //   this.setState({ showRoom: newRoom });
+  //   //then update state here, and will pass this as props to TextEdit, so it will update state of parent...
+  // }
 
   colorBorder(string) {
     var colorBorder
@@ -154,7 +87,7 @@ class TextEditContainer extends Component {
       <div>
 
         <h2>
-          Here's the TextEditContainer.
+          TextEditContainer
         </h2>
 
         {/* <Clock
@@ -162,13 +95,13 @@ class TextEditContainer extends Component {
         /> */}
 
 
-        <button id="a" name="a" onClick={this.onChoiceClick}   style={blueButton}>Choice A</button>
+        {/* <button id="a" name="a" onClick={this.onChoiceClick}   style={blueButton}>Choice A</button>
         <button id="b" name="b" onClick={this.onChoiceClick}    style={redButton}>Choice B</button>
         <button id="c" name="c" onClick={this.onChoiceClick} style={yellowButton}>Choice C</button>
 
         <p>
           Choice: {this.state.choice ? this.state.choice : 'chosen one here'}
-        </p>
+        </p> */}
 
         <button id="showRoom1" name="room1" onClick={this.onButtonClick}   style={blueButton}>Show Room 1</button>
         <button id="showRoom2" name="room2" onClick={this.onButtonClick}    style={redButton}>Show Room 2</button>
@@ -182,12 +115,8 @@ class TextEditContainer extends Component {
 
         <div style={this.colorBorder(this.state.showRoom)}>
           <TextEdit
-            // textFromContainer={this.state.text}
-            // valueFromContainer={this.state.text}
-            // handleTextChange={this.handleTextChange}
-
             showRoom={this.state.showRoom} //this is only prop that TextEdit needs!!!
-            // idFromContainer={'room3'} //this is only prop that TextEdit needs!!!
+            // handleRoomChange={this.handleRoomChange}
           />
         </div>
 
