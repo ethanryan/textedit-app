6e5115078a375247fa8377c0ab2e1034f5e16c95
ethanryan <ethanryan@gmail.com>
2018-06-07 21:01:26 -0400

    adding console logs to Yjs

--- src/components/Yjs.js
+++ src/components/Yjs.js
@@ -29,26 +29,25 @@ var connection = io(link) //need to include LINK within io()...
 
 class Yjs extends Component {
 
-  disconnectUser() {
-    console.log('calling disconnectUser...')
-    connection.disconnect() //this works! server log shows 'user left', and updates to text don't sync on reconnect...
-  }
 
   destroyUser() {
     console.log('calling destroyUser...')
-    connection.destroy()
+    connection.destroy() //this works! server log shows 'user left', and updates to text don't sync on reconnect... (calling disconnect() instead of destroy() made updates still sync.)
     console.log('USER LEFT, connection DESTROYED.')
+    console.log('after destroyUser - connection is: ', connection) // this is good info, should have looked at this before in console...
   }
+
   reconnectUser() {
-    console.log('connection is: ', connection) // this is good info, should have looked at this before in console...
+    console.log('in reconnectUser - connection is: ', connection) // this is good info, should have looked at this before in console...
     // console.table(connection) // this is good info, should have looked at this before in console...
     // console.log('typeof connection is: ', typeof connection ) // this is good info, should have looked at this before in console...
     console.log('Object.getOwnPropertyNames(connection) is: ', Object.getOwnPropertyNames(connection) ) // this is good info, should have looked at this before in console...
-    console.log(Object.getOwnPropertyNames(connection).filter(function (p) {
-        return typeof connection[p] === 'function';
-    }));
+
+
 
     console.log('calling reconnectUser...')
+
+    // connection._onConnect()
 
     // connection.io()
     // connection.reconnect() //this isn't a function...
@@ -59,7 +58,6 @@ class Yjs extends Component {
   disconnectRoom = event => {
     const buttonValue = event.target.name
     console.log('Yjs - disconnectRoom called -->>> buttonValue: ', buttonValue)
-    // this.disconnectUser() //calling function above...
     this.destroyUser() //calling function above...
   }
 
@@ -74,11 +72,21 @@ class Yjs extends Component {
 
   componentDidMount() {
     console.log('Yjs - componentDidMount - this.props is: ', this.props)
+    console.log('yjs - componentDidMount - connection.id is: ', connection.id) // this is good info, should have looked at this before in console...
+
     // console.log('Yjs - componentDidMount - Y is: ', Y)
   } //componentDidMount
 
   render() {
     // var currentRoom = getShowRoom(this.props)
+    console.log('yjs - render - connection is: ', connection) // this is good info, should have looked at this before in console...
+    console.log('yjs - render - connection.id is: ', connection.id) // this is good info, should have looked at this before in console...
+    console.log('yjs - render - connection.ids is: ', connection.ids) // this is good info, should have looked at this before in console...
+    console.log('yjs - render - connection._yjs_connection_counter is: ', connection._yjs_connection_counter) // this is good info, should have looked at this before in console...
+    console.log('yjs - render - connection.connected is: ', connection.connected) // this is good info, should have looked at this before in console...
+    console.log('yjs - render - connection.json.connected is: ', connection.json.connected) // this is good info, should have looked at this before in console...
+    console.log('Object.getOwnPropertyNames(connection) is: ', Object.getOwnPropertyNames(connection) ) // this is good info, should have looked at this before in console...
+    ///note: above logs work after i update state.... try doing this somewhere besides render???
 
     console.log('Yjs - render - this.props is: ', this.props)
 
@@ -101,8 +109,15 @@ class Yjs extends Component {
       // bind the textarea to a shared text element
       // y.share.textarea.bind(document.getElementById(that.currentRoom))
       y.share.textarea.bind(document.getElementById(that.props.showRoom))
+      console.log('HELLO from y ~?~?~?~?~??~?~??~?~?~~~?~??~?~?~?~?~?~?~?~?~?~?~??~~!!!')
+      console.log('y is: ', y)
+      console.log('typeof y is: ', typeof y)
+      console.log('Object.getOwnPropertyNames(y) is: ', Object.getOwnPropertyNames(y) ) // this is good info, should have looked at this before in console...
+
+      console.log('yjs - in promise - connection.id is: ', connection.id) // this is good info, should have looked at this before in console...
       // y.share.textarea.bind(document.querySelector('textarea')) //this will show only first room...
     })
+
 
     return (
       <div className="Yjs-style">
@@ -118,7 +133,7 @@ class Yjs extends Component {
         </p>
 
         <button onClick={this.disconnectRoom} name={this.props.showRoom}>
-          Destroy (not disconnect) {this.props.showRoom}
+          Disconnect {this.props.showRoom}
         </button>
 
         <button onClick={this.reconnectRoom} name={this.props.showRoom}>
