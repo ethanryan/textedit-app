b43a5faafac70fd2cd46594815a42e69fe03b8da
ethanryan <ethanryan@gmail.com>
2018-07-30 11:52:34 -0400

    renamed QuillEditorSnow to QuillEditor

---
+++ src/components/QuillEditor.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import YjsQuillSnow from './YjsQuillSnow';
+
+class QuillEditor extends React.Component {
+
+  componentDidMount() {
+    console.warn('1. QuillEditor - componentDidMount...')
+
+  } //componentDidMount
+
+  render() {
+    console.warn('0. QuillEditor - render...')
+    return (
+      <div className='TextEdit-style'>
+
+        <p>
+          <span style={this.props.handleColorBorder(this.props.showRoom)}>
+            QuillEditor: {this.props.showRoom}
+          </span>
+        </p>
+
+        <YjsQuillSnow
+          showRoom={this.props.showRoom} //this is only prop that TextEdit needs!!!
+          connectionExists={this.props.connectionExists}
+          handleColorBorder={this.props.handleColorBorder}
+        />
+
+        {
+          (this.props.connectionExists === true) ?
+          /* <!-- Create the editor container --> */
+          <div id="editor">
+            {/* <div className="progress"> */}
+              {/* <div className="indeterminate"></div> */}
+            {/* </div> */}
+          </div>
+          :
+          "Loading..."
+        }
+
+        {/* <div className="standalone-container">
+          <div id="QuillEditor-container">
+            <div id="editor">
+              <p>Hello World!</p>
+              <p>Some initial <strong>bold</strong> text</p>
+              <p></p>
+            </div>
+          </div>
+        </div> */}
+
+      </div>
+    )
+  }
+}
+
+export default QuillEditor;
