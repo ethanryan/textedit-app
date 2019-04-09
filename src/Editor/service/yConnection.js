b8446b65d74bbe8d48a71fe69779bd3d37d8365c
Israel-Laguan <israellaguan@gmail.com>
2019-04-08 21:22:24 -0500

    Updated package.json and react version

--- src/Editor/service/yConnection.js
+++ src/Editor/service/yConnection.js
@@ -9,9 +9,9 @@ require('y-websockets-client')(Y)
 var io = Y['websockets-client'].io //need to get this.....
 
 
-export var link = 'http://localhost:1234' //when running textedit-app-yjs-websockets-server locally
+// export var link = 'http://localhost:1234' //when running textedit-app-yjs-websockets-server locally
 // var link = 'http://localhost:5000' //when running `heroku local web`
-// var link = process.env.REACT_APP_YJS_HEROKU_URL //this link is set in my .env file, which is hidden from github
+export var link = process.env.REACT_APP_YJS_HEROKU_URL //this link is set in my .env file, which is hidden from github
 
 // create a connection
 export var connection = io(link) 
